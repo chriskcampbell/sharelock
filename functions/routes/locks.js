6ef9fc0b862c26fe02d7bc358a0d31f0b2afa559
Christopher Campbell <chris_campbell97@me.com>
2019-09-22 11:57:41 -0400

    time zone edit

--- functions/routes/locks.js
+++ functions/routes/locks.js
@@ -9,8 +9,8 @@ router.get('/:lockId', (req, res, next) => {
         let selectedLock = await requests.getLock(req.params.lockId);
         console.log(selectedLock);
         for (var i = 0; i < lockKeys.length; i++) {
-            lockKeys[i]['formattedExpiryTime'] = moment(lockKeys[i].expiryTime).format('MM/DD/YYYY HH:mm');
-            lockKeys[i]['lastAccessedTime'] = (lockKeys[i].lastUsedTime) ? moment(lockKeys[i].lastUsedTime).format('MM/DD/YYYY HH:mm') : 'N/A';
+            lockKeys[i]['formattedExpiryTime'] = moment(lockKeys[i].expiryTime).subtract(4, 'hours').format('MM/DD/YYYY HH:mm');
+            lockKeys[i]['lastAccessedTime'] = (lockKeys[i].lastUsedTime) ? moment(lockKeys[i].lastUsedTime).subtract(4, 'hours').format('MM/DD/YYYY HH:mm') : 'N/A';
             lockKeys[i].user.phoneNumber = phoneUtil.formatInOriginalFormat(phoneUtil.parse(lockKeys[i].user.phoneNumber, 'US'), 'US');
         }
         res.render('lockInfo', {
